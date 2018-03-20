41214fb519b09cf6a9c75d8b29ff31ddd1574e3e
mikedelong <mikedelong@outlook.com>
2018-03-20 13:00:02 -0400

    feature: read settings from a settings JSON file. feature: input filename handling.

--- code/initial_read.py
+++ code/initial_read.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import time
 
@@ -11,6 +12,26 @@ console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 console_handler.setLevel(logging.DEBUG)
 logger.debug('started')
+
+# read the input filename from a JSON file
+settings_file = './settings.json'
+logger.debug('settings file : %s' % settings_file)
+with open(settings_file, 'r') as settings_fp:
+    settings = json.load(settings_fp)
+
+logger.debug('settings: %s' % settings)
+
+for setting in ['input_folder', 'input_files']:
+    if setting not in settings.keys():
+        logger.warning('essential setting %s not found. Quitting.')
+        quit()
+
+input_folder = settings['input_folder']
+input_files = settings['input_files']
+
+for input_file in input_files:
+    full_input_file = input_folder + input_file
+    logger.debug('reading data from input file %s' % full_input_file)
 
 logger.debug('done')
 finish_time = time.time()

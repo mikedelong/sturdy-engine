e442620d8007068ed4b4cbe8538ca4693f989c7a
mikedelong <mikedelong@outlook.com>
2018-03-21 08:04:05 -0400

    feature: read down to the end of the description header feature: read the CSV out of the rest of the file, with the appropriate separator; this code works for pipe-delimited data, but fails for space-delimited data.

--- code/initial_read.py
+++ code/initial_read.py
@@ -2,6 +2,8 @@ import json
 import logging
 import time
 
+import pandas as pd
+
 start_time = time.time()
 
 formatter = logging.Formatter('%(asctime)s : %(name)s :: %(levelname)s : %(message)s')
@@ -29,9 +31,25 @@ for setting in ['input_folder', 'input_files']:
 input_folder = settings['input_folder']
 input_files = settings['input_files']
 
+# todo check that the input folder ends in a slash
+
 for input_file in input_files:
     full_input_file = input_folder + input_file
     logger.debug('reading data from input file %s' % full_input_file)
+
+    with open(full_input_file, 'r') as input_csv_fp:
+        line_count = 0
+        for row in input_csv_fp:
+            if row.startswith('>END'):
+                logger.debug('file: %s, skip to %d' % (full_input_file, line_count))
+                break
+            else:
+                line_count += 1
+    logger.debug('skip to %d' % line_count)
+    if 'MULTI' in full_input_file:
+        data = pd.read_csv(full_input_file, skiprows=line_count + 1, sep='|')
+        logger.debug('data frame from %s has shape %d x %d' % (full_input_file, data.shape[0], data.shape[1]))
+
 
 logger.debug('done')
 finish_time = time.time()

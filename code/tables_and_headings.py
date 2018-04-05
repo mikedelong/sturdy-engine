38e332e365ea15938dc138cbf221bc00081692a5
mikedelong <mikedelong@outlook.com>
2018-04-05 14:16:48 -0400

    feature: use a Counter to get the counts, then the top entries, then produce a histogram.

--- code/tables_and_headings.py
+++ code/tables_and_headings.py
@@ -1,7 +1,10 @@
 import json
 import logging
 import time
+from collections import Counter
 
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
 start_time = time.time()
@@ -15,7 +18,7 @@ console_handler.setLevel(logging.DEBUG)
 logger.debug('started')
 
 # read the input filename from a JSON file
-settings_file = './settings.json'
+settings_file = './settings-headings.json'
 logger.debug('settings file : %s' % settings_file)
 with open(settings_file, 'r') as settings_fp:
     settings = json.load(settings_fp)
@@ -26,10 +29,35 @@ input_folder = '../output/'
 input_files = settings['input_files']
 input_files = [item.replace('.txt', '.csv') for item in input_files]
 
+counts = Counter()
+file_count = 0
+limit = 2000
+
 for item in input_files:
-    full_file_name = input_folder + item
-    data = pd.read_csv(full_file_name)
-    logger.debug('%s : %s' % (item, data.columns.values))
+    if file_count < limit:
+        full_file_name = input_folder + item
+        data = pd.read_csv(full_file_name, nrows=2)
+        values = data.columns.values
+
+        logger.debug('%s : %s' % (item, values))
+        for item in values:
+            counts[item] += 1
+
+logger.debug('there are %d unique column headings ' % len(counts))
+most = counts.most_common(40)
+labels = [item[0] for item in most]
+values = [item[1] for item in most]
+logger.debug(most)
+
+indexes = np.arange(len(labels))
+width = 0.9
+
+plt.figure(figsize=(11, 8))
+plt.bar(indexes, values, width)
+plt.xticks(indexes + width * 0.5, labels, rotation=90)
+plt.tight_layout()
+
+plt.show()
 
 logger.debug('done')
 finish_time = time.time()

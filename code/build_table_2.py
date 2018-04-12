7dd70839280bdfab9b52640721e205e5c38a81fa
mikedelong <mikedelong@outlook.com>
2018-04-12 12:48:18 -0400

    cleanup: remove a spurious comment.

--- code/build_table_2.py
+++ code/build_table_2.py
@@ -14,7 +14,6 @@ logger.addHandler(console_handler)
 console_handler.setLevel(logging.DEBUG)
 logger.debug('started')
 
-# read the input filename from a JSON file
 settings_file = './settings-table-2.json'
 logger.debug('settings file : %s' % settings_file)
 with open(settings_file, 'r') as settings_fp:

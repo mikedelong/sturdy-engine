24b67ca5e2d497e28d3ef4b60fb645ecdc40c1cb
mikedelong <mikedelong@outlook.com>
2018-04-17 10:33:27 -0400

    feature: read files and get just the column we're interested in.

--- code/build_table_6.py
+++ code/build_table_6.py
@@ -22,17 +22,18 @@ with open(settings_file, 'r') as settings_fp:
 logger.debug('settings: %s' % settings)
 
 headings_of_interest = settings['headings_of_interest']
-input_file = settings['input_file']
+input_files = settings['input_files']
 input_folder = settings['input_folder']
-full_input_file = input_folder + input_file
-logger.debug('input file : %s' % full_input_file)
-data = pd.read_csv(full_input_file, nrows=2)
-headings = data.columns.values
-logger.debug('headings: %s' % headings)
-data = pd.read_csv(full_input_file, usecols=headings_of_interest)
-logger.debug('data has shape %d x %d' % data.shape)
-logger.debug('data has counts: \n%s' % data.count())
-logger.debug('data head: \n%s' % data.head(20))
+for input_file in input_files:
+    full_input_file = input_folder + input_file
+    logger.debug('input file : %s' % full_input_file)
+    data = pd.read_csv(full_input_file, nrows=2)
+    headings = data.columns.values
+    logger.debug('headings: %s' % headings)
+    data = pd.read_csv(full_input_file, usecols=headings_of_interest)
+    logger.debug('data has shape %d x %d' % data.shape)
+    logger.debug('data has counts: \n%s' % data.count())
+    logger.debug('data head: \n%s' % data.head(20))
 
 logger.debug('done')
 finish_time = time.time()
